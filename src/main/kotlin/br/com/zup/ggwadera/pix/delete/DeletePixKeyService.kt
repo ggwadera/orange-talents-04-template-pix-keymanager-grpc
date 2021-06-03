aaa6619c2b75238086f706ce59b2245f023e143b
Guilherme Gwadera <16023489+ggwadera@users.noreply.github.com>
2021-06-03 15:54:40 -0300

    deletar chave no bcb

--- src/main/kotlin/br/com/zup/ggwadera/pix/delete/DeletePixKeyService.kt
+++ src/main/kotlin/br/com/zup/ggwadera/pix/delete/DeletePixKeyService.kt
@@ -1,5 +1,8 @@
 package br.com.zup.ggwadera.pix.delete
 
+import br.com.zup.ggwadera.bcb.BcbClient
+import br.com.zup.ggwadera.bcb.DeletePixKeyRequest
+import br.com.zup.ggwadera.pix.PixKey
 import br.com.zup.ggwadera.pix.PixKeyRepository
 import br.com.zup.ggwadera.util.exception.NotFoundException
 import io.micronaut.validation.Validated
@@ -9,12 +12,23 @@ import javax.validation.constraints.NotBlank
 
 @Singleton
 @Validated
-class DeletePixKeyService(private val pixKeyRepository: PixKeyRepository) {
+class DeletePixKeyService(
+    private val pixKeyRepository: PixKeyRepository,
+    private val bcbClient: BcbClient
+) {
     fun delete(@NotBlank clientId: String, @NotBlank pixId: String) {
-        val pixKey = pixKeyRepository.findByUuidAndClientId(
-            uuid = UUID.fromString(pixId),
-            clientId = UUID.fromString(clientId)
-        ) ?: throw NotFoundException("chave não existe ou não pertence ao usuário")
+        val pixKey = findKey(pixId, clientId)
+        sendKeyDeleteRequest(pixKey)
         pixKeyRepository.delete(pixKey)
+    }
+
+    private fun findKey(pixId: String, clientId: String) = pixKeyRepository.findByUuidAndOwnerId(
+        uuid = UUID.fromString(pixId),
+        ownerId = UUID.fromString(clientId)
+    ) ?: throw NotFoundException("chave não existe ou não pertence ao usuário")
+
+    private fun sendKeyDeleteRequest(pixKey: PixKey) = with(pixKey) {
+        bcbClient.deleteKey(key, DeletePixKeyRequest(key, account.participant))
+            ?: throw IllegalStateException("Falha ao apagar registro no Banco Central")
     }
 }
